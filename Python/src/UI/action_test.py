95521736675a84ee85ccbddabe4e9c12c08cacf9
David Tigre Moraes <tigredavid007@gmail.com>
2023-01-09 16:38:31 -0300

    Testes Action UI

---
+++ Python/src/UI/action_test.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+from unittest import main, TestCase
+from src.UI.action import ObterInputUsuario
+
+class ActionTests(TestCase):
+    @patch('builtins.input', lambda _: '2')
+    def test_Quando_ObterImputUsuario_Deve_RetornarONumeroInseridoPeloUsuario(self):
+        assert ObterInputUsuario() == 2
+    
+    @patch('builtins.input', side_effect=['E', '+', '2'])
+    def test_Dado_VariasEntradasDoUsuario_Quando_ObterImputUsuario_Deve_RetornarSomenteAquelaQueEhUmNumero(self, patched):
+        assert ObterInputUsuario() == 2
+        assert patched.call_count == 3
+
+if __name__ == '__main__':
+    main()
